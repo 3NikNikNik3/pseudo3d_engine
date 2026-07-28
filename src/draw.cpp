4f14c2f3b43c6d85249610369ac1dc4044929b46
3NikNikNik3 <>
2026-07-28 09:23:30 +0300

    On draw change Rect to Vertex

--- src/draw.cpp
+++ src/draw.cpp
@@ -3,13 +3,13 @@
 namespace pseudo3d_engine {
 	namespace draw {
 		void draw_line(Window &win, int x, int y, int size, uchar r, uchar g, uchar b, uchar a) {
-			static sf::RectangleShape rect({1, 1});
+			sf::Vertex arr[4] = {
+				{ { (float)x, (float)y }, { r, g, b, a } },
+				{ { x + 1.0f, (float)y }, { r, g, b, a } },
+				{ { (float)x, (float)y + size }, {r, g, b, a } },
+				{ { x + 1.0f, (float)y + size }, {r, g, b, a } } };
 
-			rect.setSize({1, (float)size});
-			rect.setPosition({(float)x, (float)y});
-			rect.setFillColor({r, g, b, a});
-
-			win.window->draw(rect);
+			win.window->draw(arr, 4, sf::PrimitiveType::TriangleStrip);
 		}
 	}
 }
